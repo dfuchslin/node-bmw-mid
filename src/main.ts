a00b27eb7dc50b207861b09483004138bea9c8d1
David Füchslin <david.fuchslin@gmail.com>
2023-01-08 17:54:39 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,34 @@
 console.log('Hello world!');
 
 const something = 'some string';
+const state = {};
 
-const d = () => {
-  console.log('do something here');
+const init_signal_listeners = async () => {
+  process.on('SIGTERM', async () => {
+    console.log('Caught SIGTERM');
+    await term();
+  });
+
+  process.on('SIGINT', async () => {
+    console.log('Caught SIGINT');
+    await term();
+  });
+
+  process.on('exit', () => {
+    console.log('Exiting');
+  });
 };
+
+const init = async () => {
+  console.log('Initializing');
+
+  await init_signal_listeners();
+};
+
+const term = async () => {
+  console.log('Terminating');
+};
+
+(async () => {
+  await init();
+})();
