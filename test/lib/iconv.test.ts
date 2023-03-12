08d7f23e447e5d15f514afb6db551b642b8f721b
David Füchslin <david.fuchslin@gmail.com>
2023-03-12 20:28:15 +0100

    update test

--- test/lib/iconv.test.ts
+++ test/lib/iconv.test.ts
@@ -7,7 +7,7 @@ describe('iconv', () => {
     expect(result).toBe('R\xa4ksm\xa5rgaas');
   });
   test('remove diacriticals', () => {
-    const result = iconv.utf8_ibusascii(`Plus ça change, plus c'est la même chose. 42`);
-    expect(result).toBe(`Plus ca change, plus c'est la meme chose. 42`);
+    const result = iconv.utf8_ibusascii(`Plus ça change, plus c'est la même chose. 42!`);
+    expect(result).toBe(`Plus ca change, plus c'est la meme chose. 42!`);
   });
 });
