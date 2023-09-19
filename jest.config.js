e4b28ef422823fa0467210d21f04e41551bfd710
David Füchslin <david.fuchslin@gmail.com>
2023-09-19 21:08:48 +0200

    various cleanup, ts paths, upgrades

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: './test',
--- src/ibus/router.ts
+++ src/ibus/router.ts
@@ -1,26 +1,36 @@
 import Logger from '@/lib/log';
 import { IbusInterface } from '@/lib/ibus';
 import { Device, FullIbusMessage, IbusDeviceId } from '@/types';
-import { CDC, IKE, MID, RAD, TEL } from '@/ibus/devices';
+//import { CDC, IKE, MID, RAD, TEL } from '@/ibus/devices';
+import * as AllDevices from '@/ibus/devices';
 import { deviceStatus } from '@/ibus/message';
+import { EventBus } from '@/eventbus';
+import cdcnew from '@/ibus/devices/IbusDevice';
 
 const context = 'ibus-router';
 const log = Logger.get(context);
 let ibusInterface: IbusInterface;
 
-const registeredDevices: Record<number, Device> = {
-  [CDC.id]: CDC,
-  [IKE.id]: IKE,
-  [MID.id]: MID,
-  [RAD.id]: RAD,
-  [TEL.id]: TEL,
-};
+const registeredDevices: Record<number, Device> = {};
+//const registeredDevices: Record<number, Device> = {
+//  [CDC.id]: CDC,
+//  [IKE.id]: IKE,
+//  [MID.id]: MID,
+//  [RAD.id]: RAD,
+//  [TEL.id]: TEL,
+//};
 
-const init = async (_ibusInterface: IbusInterface) => {
-  ibusInterface = _ibusInterface;
+const init = async (config: { ibusInterface: IbusInterface; eventBus: EventBus }) => {
+  ibusInterface = config.ibusInterface;
   ibusInterface.on('data', routeMessage, { context });
   log.notice('Initialized ibus message router');
-  Object.values<Device>(registeredDevices).forEach((device) => device.init(ibusInterface));
+
+  Object.values(AllDevices).forEach((device) => (registeredDevices[device.id] = device));
+
+  const CDCNEW = new cdcnew(config);
+  registeredDevices[CDCNEW.id] = CDCNEW;
+
+  Object.values<Device>(registeredDevices).forEach((device) => device.init(config.ibusInterface));
 };
 
 const routeMessage = (message: FullIbusMessage) => {
