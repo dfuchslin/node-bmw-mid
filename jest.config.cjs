3604cb45dfe5fe7f169320d0d5efc9f5d1524150
David Füchslin <david.fuchslin@gmail.com>
2024-11-24 15:58:14 +0100

    cleanup

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: './test',
