c7c65ada7eccdabb54b92ae9a40adbc121949d3e
David Füchslin <david.fuchslin@gmail.com>
2024-11-24 15:53:55 +0100

    eventbus

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,16 +4,23 @@ import json from 'koa-json';
 import Logger from '@/lib/log';
 import { config } from '@/config';
 import router from '@/api/routes';
+import { EventBus } from '@/eventbus';
 
 const namespace = 'api';
 const log = Logger.get(namespace);
 const app = new Koa();
-const port = config.api.port;
-app.use(bodyParser());
-app.use(json());
-app.use(router.routes()).use(router.allowedMethods());
+// const port = config.api.port;
+// app.use(bodyParser());
+// app.use(json());
+// app.use(router.routes()).use(router.allowedMethods());
 
-const init = async () => {
+const init = async (eventBus: EventBus) => {
+  const port = config.api.port;
+
+  app.use(bodyParser());
+  app.use(json());
+  app.use(router.routes()).use(router.allowedMethods());
+
   app.listen(port, () => {
     log.notice(`API server started on port ${port}`);
   });
