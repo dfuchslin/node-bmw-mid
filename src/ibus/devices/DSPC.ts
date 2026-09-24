1e0b7e4a36a6496c241a9d0d0bb9e0806f28265d
David Füchslin <david.fuchslin@gmail.com>
2026-09-24 15:46:22 +0200

    add dummy dsp devices

---
+++ src/ibus/devices/DSPC.ts
@@ -0,0 +1,29 @@
+import { IbusInterface } from '../../lib/ibus/index.js';
+import Logger from '../../lib/log.js';
+import { Device, FullIbusMessage, IbusDeviceId } from '../../types/index.js';
+
+const id = IbusDeviceId.DSPC;
+const context = IbusDeviceId[id].toLowerCase();
+const log = Logger.get(context);
+
+const init = (_ibusInterface: IbusInterface) => {
+  log.notice('init');
+};
+
+const term = () => {
+  log.notice('term');
+};
+
+const parseMessage = (message: FullIbusMessage) => {
+  switch (message.msg[0]) {
+    default:
+      log.warn('Unhandled message!', message.msg);
+  }
+};
+
+export const DSPC: Device = {
+  id,
+  init,
+  term,
+  parseMessage,
+};
--- src/ibus/devices/index.ts
+++ src/ibus/devices/index.ts
@@ -1,4 +1,6 @@
 export * from './CDC.js';
+export * from './DSP.js';
+export * from './DSPC.js';
 export * from './IKE.js';
 export * from './MID.js';
 export * from './TEL.js';
